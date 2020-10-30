4da22688cf47598a25c6c3139290c1b7f6b34213
Zhou Hao <hao.zhou@cognizant.com>
2020-10-30 15:11:48 +0800

    Add db support

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-import express from 'express'
 import config from './config'
 import app from './app'
+import mongoose from 'mongoose'
 
 const start = () => {  
-  return app.listen(config.port, () => {
+  // start server
+  const server = app.listen(config.port, () => {
     console.log(`Start server at ${server.address().port}`)
   })    
+
+  // start database
+  // const db = process.env.DATABASE.replace(
+  //   '<PASSWORD>',
+  //   process.env.DATABASE_PASSWORD
+  // )
+  
+  mongoose
+    .connect(config.databaseURL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    })
+    .then(() => console.log('DB connection successful!'));  
 }
 
-const server = start()
+start()
